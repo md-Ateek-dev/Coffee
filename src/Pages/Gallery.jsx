045648475777bf0583a699a7311bed80dec8b8db
Mohd Ateek <mohdateek.dev@gmail.com>
2026-08-02 12:33:00 +0530

    fix import

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,11 +1,11 @@
-import Navbar from "@/Components/Comman/Navbar";
-import Footer from "@/Components/Comman/Footer";
-import PageTransition from "@/Components/Comman/PageTransition";
-import CoffeeMoments from "@/Components/Gallery/CoffeeMoments";
-import GalleryHero from "@/Components/Gallery/GalleryHero";
-import InstagramFeed from "@/Components/Gallery/InstagramFeed";
-import MasonryGallery from "@/Components/Gallery/MasonryGallery";
-import VideoGallery from "@/Components/Gallery/VideoGallery";
+import Navbar from "../Components/Comman/Navbar";
+import Footer from "../Components/Comman/Footer";
+import PageTransition from "../Components/Comman/PageTransition";
+import CoffeeMoments from "../Components/Gallery/CoffeeMoments";
+import GalleryHero from "../Components/Gallery/GalleryHero";
+import InstagramFeed from "../Components/Gallery/InstagramFeed";
+import MasonryGallery from "../Components/Gallery/MasonryGallery";
+import VideoGallery from "../Components/Gallery/VideoGallery";
 
 const Gallery = () => {
   return (
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,11 +7,11 @@ import Hero from "../Components/Home/Hero/Hero";
 import BrandStory from "../Components/Home/BrandStory";
 import FeaturedCoffee from "../Components/Home/FeaturedCoffee";
 import CoffeeJourney from "../Components/Home/CoffeeJourney";
-import BestSellers from "@/Components/Home/BestSellers";
-import WhyChooseUs from "@/Components/Home/WhyChooseUs";
-import Testimonials from "@/Components/Home/Testimonials";
-import CTA from "@/Components/Home/CTA";
-import PageTransition from "@/Components/Comman/PageTransition";
+import BestSellers from "../Components/Home/BestSellers";
+import WhyChooseUs from "../Components/Home/WhyChooseUs";
+import Testimonials from "../Components/Home/Testimonials";
+import CTA from "../Components/Home/CTA";
+import PageTransition from "../Components/Comman/PageTransition";
 
 const Home = () => {
 	return (
